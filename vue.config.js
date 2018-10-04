8bc84f2d6af9350085f5f93b5b0e527755ab49a2
cwzp990 <cwzp990@163.com>
2018-10-04 22:46:23 +0800

    webpack config

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,7 @@
-const path = require('path')
-
-function resolve(dir) {
-  return path.join(__dirname, '..', dir)
-}
+// const path = require('path')
+// function resolve(dir) {
+//   return path.join(__dirname, '..', dir)
+// }
 
 module.exports = {
   lintOnSave: false,
@@ -21,13 +20,22 @@ module.exports = {
   chainWebpack: config => {
     // config.resolve.alias
     //     .set('public', resolve('public'))
-    config.module.rule('svg').exclude.add(resolve('src/icons'))
-    config.module
-      .rule('svg-icon')
-      .test(/\.svg$/)
-      .include.add(resolve('src/icons'))
-      .end()
-      .use('svg-sprite')
+    // 别人那里复制的，可以，我这里不行，不知是什么原因
+    // config.module.rule('svg').exclude.add(resolve('src/icons'))
+    // config.module
+    //   .rule('svg-icon')
+    //   .test(/\.svg$/)
+    //   .include.add(resolve('src/icons'))
+    //   .end()
+    //   .use('svg-sprite-loader')
+    //   .loader('svg-sprite-loader')
+    //   .options({
+    //     symbolId: 'icon-[name]'
+    //   })
+    const svgRule = config.module.rule('svg')
+    svgRule.uses.clear()
+    svgRule
+      .use('svg-sprite-loader')
       .loader('svg-sprite-loader')
       .options({
         symbolId: 'icon-[name]'
